b3fa1748f148c4759857024eca558d0e531f6870
w-michelle <89858197+w-michelle@users.noreply.github.com>
2024-06-25 17:38:12 -0400

    configure vercel.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,12 +20,6 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app);
-
-server.listen("8080", () => {
-  console.log("Server running on http://localhost:8080/");
-});
-
 const MONGO_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_KEY}@cluster0.v8jjewz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.Promise = Promise;
@@ -34,4 +28,4 @@ mongoose.connection.on("error", (error: Error) => console.log(error));
 
 app.use("/", router());
 
-module.exports = server;
+module.exports = app;
