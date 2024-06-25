17027113e2ff21d0dc660aead984183134046486
w-michelle <89858197+w-michelle@users.noreply.github.com>
2024-06-25 16:57:23 -0400

    Add vercel.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,9 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen("https://cbxworld-server.vercel.app");
+server.listen("8080", () => {
+  console.log("Server running on http://localhost:8080/");
+});
 
 const MONGO_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_KEY}@cluster0.v8jjewz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,3 +33,5 @@ mongoose.connect(MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
 
 app.use("/", router());
+
+module.exports = server;
