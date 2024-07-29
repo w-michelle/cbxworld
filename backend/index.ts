2d17b8d7535244c8933bbc9dfaed827adb53bd27
w-michelle <89858197+w-michelle@users.noreply.github.com>
2024-07-29 09:58:42 -0400

    modify allowed methods

--- backend/index.ts
+++ backend/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: "https://cbxworld.vercel.app",
-    methods: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
